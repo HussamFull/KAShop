b2ea856c2d78f2e57dc0956359216364d7154e1c
hamada <bhak6150@gmail.com>
2025-06-21 14:22:35 +0200

    ADD Dashbord

---
+++ KAShop/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using KAShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace KAShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+    public class HomeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+
+            // ViewBag.categories = context.Categories.ToList();
+            // ViewBag.products = context.Products.ToList();
+            var categories = context.Categories.ToList();
+            return View(categories);
+            
+        }
+    }
+}
