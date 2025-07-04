c7de175a09ba037bbf862178d6055121274b1c13
hamada <bhak6150@gmail.com>
2025-07-04 22:09:04 +0200

    Add Validate to Cat and Prod

--- KAShop/Models/Category.cs
+++ KAShop/Models/Category.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace KAShop.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Name { get; set; }
 
 
