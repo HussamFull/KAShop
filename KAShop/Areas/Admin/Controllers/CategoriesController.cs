8f7ead6da4ea204648f812a2e4a98e5839495b84
hamada <bhak6150@gmail.com>
2025-07-06 12:35:59 +0200

    Add Sweetalert and Add Edit for Cat

--- KAShop/Areas/Admin/Controllers/CategoriesController.cs
+++ KAShop/Areas/Admin/Controllers/CategoriesController.cs
@@ -55,5 +55,37 @@ namespace KAShop.Areas.Admin.Controllers
             return View();
         }
 
+        // GET: Admin/Categories/Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                TempData["message"] = "Category not found.";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        // POST: Admin/Categories/Edit
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Edit(Category request)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = context.Categories.FirstOrDefault(c => c.Id == request.Id);
+                if (category != null)
+                {
+                    category.Name = request.Name;
+                    context.SaveChanges();
+                    TempData["success"] = "Category updated successfully.";
+                    return RedirectToAction("Index");
+                }
+                TempData["message"] = "Category not found.";
+            }
+            return View(request);
+
+        }
     }
 }
